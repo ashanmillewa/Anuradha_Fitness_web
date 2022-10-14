eb61bb4356c2da64d094990fc3ed9c3945f90de8
Ashan Millewa <ashanmillewa@gmail.com>
2022-10-14 12:54:48 +0530

    home

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className='Home'>
+      <div className="left-h">
+        left side 
+
+      </div>
+
+      <div className="left-h">
+        right side
+        
+      </div>
+      
+    </div>
+  );
+}
+
+export default Home;
