33a44b1e315a0e82945facacd8f1f2460d092afd
Ashan Millewa <ashanmillewa@gmail.com>
2022-10-23 21:18:35 +0530

    program1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 
 import Home from './components/Home/Home';
+import Program from './components/Program/Program';
 
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <Program/>
 
 
     </div>
