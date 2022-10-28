73bdac5bf81e284e33db7c8bc79accbe293773da
Ashan Millewa <ashanmillewa@gmail.com>
2022-10-28 22:24:46 +0530

    testimonial1
    1

--- src/components/Plans/Plans.jsx
+++ src/components/Plans/Plans.jsx
@@ -2,39 +2,7 @@ import React from 'react';
 import {plansData} from '../../data/plansData';
 import './Plans.css';
 
-import whiteTick from "../../assets/whiteTick.png";
-
-function Plans() {
-  return (
-    <div className="plans-container">
-        <div className="programs-header" style={{gap:'1.5rem'}}>
-            <span className="stroke-text">READY TO START</span>
-            <span>YOUR JOURNEY</span>
-            <span className="stroke-text">NOW WITHUS</span>
-        </div>
-        {/*plans card*/}
-        <div className="plans">
-            {plansData.map((plan,i)=>(
-                <div className="plan" key={i}>
-                    {plan.icon}
-                    <span>{plan.name}</span>
-                    <span>${plan.price}</span>
-
-                    <div className="features">
-                        {plan.features.map((feature,i)=>(
-                            <div className="feature">
-                                <img src={whiteTick} alt="" />
-                                <span key={i}>{feature}</span>
-                            </div>
-                        ))}
-                    </div>
-
-                    <div>
-                        <span>see more beneficts</span>
-                        <button className="btn">Join now</button>
-                    </div>
-
-                </div>
+import w
             ))}            
         </div>
     </div>
