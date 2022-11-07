67d01cb2447a6a127d161e392d46bbdf8ece25a3
Ashan Millewa <ashanmillewa@gmail.com>
2022-11-07 21:14:10 +0530

    join4

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -6,6 +6,21 @@ function Join() {
   return (
     <div className='Join' id='join-us'>
         <div className="left-j"> 
+        <div>
+          <span>READY TO</span>
+          <span>LEVEL UP</span>
+        </div>
+        <div>
+          <span>YOUR BODY</span>
+          <span>WITH US?</span>
+        </div>
+    </div>
+
+    <div className="right-j">
+      <form action="" className="email-container">
+        <input type="email" name="user_email" placeholder="enter your email address" />
+      </form>
+    </div>
     </div>
   );
 }
