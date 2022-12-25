957b78276ecb75f10adb56317911657a34a69b5c
Ashan Millewa <ashanmillewa@gmail.com>
2022-12-26 00:30:40 +0530

    01.54_update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,15 +8,24 @@ import home_image_back from "../../assets/home_image_back.png";
 import heart from "../../assets/heart.png";
 import calories from "../../assets/calories.png";
 
+import {motion} from 'framer-motion';
+
 function Home() {
+
+  const transition = {type:'spring', duration:3}
   return (
     <div className='Home'>
+      <div className="blur home-blur"></div>
       <div className="left-h">
         <Header/>
 
     {/*the best ad*/}
       <div className="the-best-ad">
-        <div></div>
+        <motion.div
+        initial={{left:'238px'}}
+        whileInView={{left:'8px'}}
+        transition={{...transition,type:'tween'}}
+        ></motion.div>
         <span>the best fitness clun in the town</span>
       </div>
 
@@ -68,17 +77,30 @@ function Home() {
       <div className="right-h">
         <button className="btn">Join Now</button>
 
-        <div className="heart-rate">
+        <motion.div 
+        initial={{right:'-1rem'}}
+        whileInView={{right:'4rem'}}
+        transition={transition}
+        className="heart-rate">
           <img src={heart} alt=""/>
           <span>Heart Rate</span><span>116 bmp</span>
-        </div>
+        </motion.div>
 
 {/*home image*/}
     <img src={home_image} alt="" className="home-image"/>
-    <img src={home_image_back} alt="" className='home-image-back'/>
+    <motion.img 
+    initial={{right:'11rem'}}
+    whileInView={{right:'20rem'}}
+    transition={transition}
+    
+    src={home_image_back} alt="" className='home-image-back'/>
 
 {/*calories*/}
-    <div className="calories">
+    <motion.div 
+    initial={{right:'37rem'}}
+    whileInView={{right:'28rem'}}
+    transition={transition}    
+    className="calories">
     <img src={calories} alt="" />
 
           <div>
@@ -86,7 +108,7 @@ function Home() {
             <span>220 kcal</span>
           </div>
 
-        </div>
+        </motion.div>
       </div>
     </div>
   );
